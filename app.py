c98c8e036e426ae4d63130d381aa5bd5592dcef3
Genevieve Kochel <gigiellen@icloud.com>
2024-10-31 18:34:49 -0700

    yay progress

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 import streamlit as st 
+
+from audio_recorder_streamlit import audio_recorder
+import openai
+import base64
+
+def main():
+    st.sidebar.title("API KEY CONFIGURATION")
+    api_key = st.sidebar.text_input("Please enter your API key!: ")
